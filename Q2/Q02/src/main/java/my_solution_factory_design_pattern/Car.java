f428a3f0d9fb1e2a11216181840a0bdab811a1f3
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-01-14 14:35:22 +0600

    Modified

---
+++ Q2/Q02/src/main/java/my_solution_factory_design_pattern/Car.java
@@ -0,0 +1,17 @@
+package my_solution_factory_design_pattern;
+//Sub Class
+public class Car extends Vehicle{
+
+    public Car() {
+    }
+
+    @Override
+    public int set_num_of_passengers() {
+        return 4;
+    }
+
+    @Override
+    public boolean has_gas() {
+        return true;
+    }
+}
